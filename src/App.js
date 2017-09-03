53594cdd6d57c9536a0b39fad77bdc2c6676db65
Alexandre Sette <alexandresette@MacBook-Pro-de-Alexandre.local>
2017-09-03 19:00:30 -0300

    feat: Construindo fluxo para troca de status dos livros

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './services/api/BooksAPI';
 import HomeScene from './scenes/Home';
 import SearchScene from './scenes/Search';
 import './App.css'
 
 class BooksApp extends Component {
-  state = {}
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState((state) => ({ books }))
+    });
+  }
+
+  onBookChangerState = (evt) => (book) => {
+    BooksAPI.update(book, evt.target.value).then(books => {
+      this.setState((state) => ({ books }))
+    });
+  }
 
   render() {
     return (
       <div className="app">
         <Route exact path='/' render={() => (
-          <HomeScene />
+          <HomeScene books={this.state.books}
+            onBookChangerState={this.onBookChangerState}
+          />
         )} />
         <Route path='/search' render={({ history }) => (
           <SearchScene />
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export const Book = ({ title, authors, urlImage, bookCurrentShelf, onBookChanger }) => (
+export const Book = ({ data, onBookChangerState }) => (
     <div className="book">
         <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${urlImage}")` }}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${data.imageLinks.smallThumbnail}")` }}></div>
             <div className="book-shelf-changer">
-                <select onChange={onBookChanger} value={bookCurrentShelf}>
+                <select onChange={(e) => onBookChangerState(e)(data)} defaultValue={data.shelf}>
                     <option value="none" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
@@ -14,28 +14,21 @@ export const Book = ({ title, authors, urlImage, bookCurrentShelf, onBookChanger
                 </select>
             </div>
         </div>
-        <div className="book-title">{title}</div>
-        <div className="book-authors">{authors}</div>
+        <div className="book-title">{data.title}</div>
+        <div className="book-authors">{data.authors.join(", ")}</div>
     </div>
 );
 
-export const bookshelf = ({ title = "No title", listBooks = [], typeShelf, onBookChanger }) => (
+export const Shelf = ({ title = "No title", books = [], onBookChangerState }) => (
     <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
+        <h2 className="bookshelf-title">{title}</h2>
         <div className="bookshelf-books">
             <ol className="books-grid">
-                {{
-                    listBooks.map(book => (
-                        <li>
-                            <Book title={book.title}
-                                authors={book.authors}
-                                urlImage={book.urlImage}
-                                bookCurrentShelf={typeShelf}
-                                onBookChanger={onBookChanger}
-                            />
-                        </li>
-                    ))
-                }}
+                {books.map(book => (
+                    <li key={book.id}>
+                        <Book data={book} onBookChangerState={onBookChangerState} />
+                    </li>
+                ))}
             </ol>
         </div>
     </div>
